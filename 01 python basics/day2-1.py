fe0141c7a36984ebc16d2516cc0a516dc9b13a3a
shrivastavapriyansh2004-web <shrivastavapriyansh2004@gmail.com>
2026-09-14 17:44:23 +0530

    Day 2 - if-else and decision making completed.

---
+++ 01 python basics/day2-1.py
@@ -0,0 +1,7 @@
+num = int(input("enter the number: "))
+if num < 0:
+ print("negative")
+elif num > 0:
+ print("positive")
+else:
+ print("zero")
---
+++ 01 python basics/day2-3.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter the number: "))
+num2 = int(input("enter the number: "))
+if num1 < num2:
+    print(num2)
+elif num1 > num2:
+    print(num1)
+else:
+    print("equal")
