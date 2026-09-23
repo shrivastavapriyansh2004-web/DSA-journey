8d4cdbd569039512440e3e8acb6f7913870ad842
shrivastavapriyansh2004-web <shrivastavapriyansh2004@gmail.com>
2026-09-23 17:35:49 +0530

    day 10 - dictonaries

---
+++ 01 python basics/day10-1.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "priyansh",
+    "age": 22,
+    "grade": "graduate",
+    "city": "sironj"
+}
+student["age"] = 23
+print(student)
---
+++ 01 python basics/day10-2.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "priyansh",
+    "age": 22,
+    "grade": "graduate",
+    "city": "sironj"
+}
+student["age"] = 23
+for key, value in student.items():
+    print(f"{key}-> {value}")
+
